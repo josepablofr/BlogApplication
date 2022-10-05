1b8b52990e3873c23f7a2c50280aae0dea95cec0
Jose Pablo Flores Rabasa <josepablofr@MacBook-Pro-de-Jose.local>
2022-10-05 16:06:16 -0500

    Prueba Pepesuki

--- app/src/main/java/com/example/blogappjp/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/blogappjp/presentation/auth/AuthViewModel.kt
@@ -12,6 +12,7 @@ class AuthViewModel (private val repo: AuthRepo): ViewModel() {
 
     fun signIn(email: String, password: String) = liveData(Dispatchers.IO) {
         emit(Result.Loading())
+        // hola Pepesuki
         try {
             emit(Result.Success(repo.signIn(email, password)))
         } catch (e: Exception) {
